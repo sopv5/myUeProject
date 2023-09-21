69e8ff1557b8ef9438d5b72084da5eedebef7c63
sopv5 <zywhebt@gmail.com>
2023-09-21 22:28:26 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testProject
+
+Developed with Unreal Engine 4
---
+++ Source/testProject/testProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class testProject : ModuleRules
+{
+	public testProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
